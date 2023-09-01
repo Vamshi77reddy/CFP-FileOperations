b08ae89a6a861fda7c8c375336d345a4d1478752
Vamshi77reddy <vamshireddy6563@gmail.com>
2023-09-01 11:56:08 +0530

    File IO Operations

---
+++ FileOperations/Fileio.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace FileOperations
+{
+    internal class Fileio
+    {
+        public void io()
+        {
+            FileStream F = new FileStream("test.txt", FileMode.OpenOrCreate,
+           FileAccess.ReadWrite);
+
+            for (int i = 1; i <= 20; i++)
+            {
+                F.WriteByte((byte)i);
+            }
+            F.Position = 0;
+            for (int i = 0; i <= 20; i++)
+            {
+                Console.Write(F.ReadByte() + " ");
+            }
+            F.Close();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ FileOperations/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+
+namespace FileOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* String path = @"C:\Users\Teju Reddy\Desktop\vamshi.txt";
+
+
+             String copypath = @"D:\ExampleNew.txt";
+
+             File.Copy(path, copypath);
+
+             Console.ReadKey();*/
+            Fileio fileio = new Fileio();
+            fileio.io();
+        }
+    }
+}
